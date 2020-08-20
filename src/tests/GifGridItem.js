a327df15851577db26d6287f9987a8d6dcf27a67
Gaston82 <cabezaartaza@hotmail.com>
2020-08-20 03:04:24 +0200

    add GifGrid test snapshot

--- src/tests/GifGridItem.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { shalow } from "enzyme";
-import GifGridItem from "../components/GifGridItem";
-
-describe("Pruebas en <GifGridItem", () => {
-  test("should show <GifGridItem/> correctly", () => {
-    const wrapper = shalow(<GifGridItem />);
-  });
-});
