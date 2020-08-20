1cd3dc74a470195ce85222bc9d7f3136c0ac5c8a
Gaston82 <cabezaartaza@hotmail.com>
2020-08-20 03:29:21 +0200

    add proptypes at GifGridItem

--- src/tests/components/GifGridItem.test.js
+++ src/tests/components/GifGridItem.test.js
@@ -3,8 +3,11 @@ import { shallow } from "enzyme";
 import { GifGridItem } from "../../components/GifGridItem";
 
 describe("Pruebas en <GifGridItem", () => {
+  const title = "A title";
+  const url = "http://localhost/algo.jpg";
+
   test("should show <GifGridItem/> correctly", () => {
-    const wrapper = shallow(<GifGridItem />);
+    const wrapper = shallow(<GifGridItem title={title} url={url} />);
     expect(wrapper).toMatchSnapshot();
   });
 });
