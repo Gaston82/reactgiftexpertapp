a293dabcb4a209eff92fa037b12d22b1faea3c1f
Gaston82 <cabezaartaza@hotmail.com>
2020-08-20 04:10:51 +0200

    add GifGridItem test img and p

--- src/tests/components/GifGridItem.test.js
+++ src/tests/components/GifGridItem.test.js
@@ -5,9 +5,18 @@ import { GifGridItem } from "../../components/GifGridItem";
 describe("Pruebas en <GifGridItem", () => {
   const title = "A title";
   const url = "http://localhost/algo.jpg";
+  const wrapper = shallow(<GifGridItem title={title} url={url} />);
 
   test("should show <GifGridItem/> correctly", () => {
-    const wrapper = shallow(<GifGridItem title={title} url={url} />);
     expect(wrapper).toMatchSnapshot();
+  });
+  test("should have a paragraph with a title", () => {
+    const p = wrapper.find("p");
+    expect(p.text().trim()).toBe(title);
+  });
+  test("image url and title should be equal to the props values", () => {
+    const img = wrapper.find("img");
+    expect(img.prop("src")).toBe(url);
+    expect(img.prop("alt")).toBe(title);
   });
 });
