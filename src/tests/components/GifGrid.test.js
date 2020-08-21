de534f1695d6c38bbd6b22856d60ec77f5aa5405
Gaston82 <cabezaartaza@hotmail.com>
2020-08-22 01:00:38 +0200

    add GifGrid test

---
+++ src/tests/components/GifGrid.test.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { shallow } from "enzyme";
+import { GifGrid } from "../../components/GifGrid";
+import { useFetchGifs } from "../../hooks/useFetchGifs";
+jest.mock("../../hooks/useFetchGifs");
+
+describe("Testing <GifGrid/>", () => {
+  const category = "spinetta";
+  test("should show <GifGrid/> correctly", () => {
+    useFetchGifs.mockReturnValue({
+      data: [],
+      loading: true,
+    });
+    const wrapper = shallow(<GifGrid category={category} />);
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  test("should show items when data is avaible", () => {
+    const gifs = [
+      {
+        id: "ABCD",
+        url: "http://localhost/test/something",
+        title: "Something",
+      },
+    ];
+    useFetchGifs.mockReturnValue({
+      data: gifs,
+      loading: false,
+    });
+    const wrapper = shallow(<GifGrid category={category} />);
+    expect(wrapper).toMatchSnapshot();
+    expect(wrapper.find("p").exists()).toBe(false);
+    expect(wrapper.find("GifGridItem").length).toBe(gifs.length);
+  });
+});
